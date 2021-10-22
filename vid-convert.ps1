4eb2fbae9caefbe2a3fbfba5db17369e0cbe2466
Brycen Medart <brycemedart@gmail.com>
2021-10-22 00:02:46 -0700

    * Converted Script from bash to powershell * Deleted uneccessary files

--- vid-convert.ps1
+++ vid-convert.ps1
@@ -5,16 +5,14 @@
 
 ##--------------
 # Argument Variables - Do not change unless use case is understood
+# Variables for path, category and full file path arguments
 ##--------------------
-
-#Variables for path, category and full file path arguments
-
 $path = $Args[0]
 $cat = $Args[1]
 $fpath = $Args[2]
 
-#Array of Movie ext types
-$ext_array = @(".avi", ".flv", ".mkv", ".mov", ".mp4", ".wmv",".srt",".en.srt")
+#Array of file types
+$ext_array = @(".avi", ".flv", ".mkv", ".mov", ".mp4", ".wmv", ".srt", ".en.srt")
 
 ##-------------
 #Global change variables - Change to configure for your environment.
@@ -24,35 +22,89 @@ $ext_array = @(".avi", ".flv", ".mkv", ".mov", ".mp4", ".wmv",".srt",".en.srt")
 $del_file = 1
 
 #Video File Save Folder, alter to save converted movies to new location.
-$saveFolder = "/home/brymed/Videos/temp"
+$ffmpegLoc = "C:\Users\bryce\Videos\TOR\ffmpeg"
+$winSCPLoc = "C:\Users\bryce\Videos\TOR\WinSCP-5.19.3-Portable"
 
 #Remote Server Information For SFTP Transfer. Alter these values to match your environment.
 $enable_transfer = 1
-$rem_ip = "192.168.1.121"
-$rem_username = "pi"
 $rem_saveFolder = "/media/PIPLEX"
+function transfer_file($ft) {
 
+    #Call winscp and pass parameters
+    & "$winSCPLoc\WinSCP.com" `
+        /log="$winSCPLoc\WinSCP.log" /ini=nul `
+        /command `
+        "open sftp://pi:Brymed%232020@192.168.1.121/ -hostkey=`"`"ssh-ed25519 255 Kdrw/Wlbl2hUooxtp4idYQUllH54YYSXT8O8UtKb2hw=`"`"" `
+        "put $fpath\$ft $rem_saveFolder/$cat/" `
+        "exit"
 
+    $winscpResult = $LastExitCode
+    if ($winscpResult -eq 0) {
+        Write-Host "Success"
+    }
+    else {
+        Write-Host "Error"
+    }
+
+    exit $winscpResult
+
+    #Remove file after transfer if $remove_file is set
+    Start-Sleep -s 1
+    if ($del_file -eq 1) {
+        Remove-Item $fpath\$ft
+    }
+}
+
+function convert_file($file) {
+
+    $fileBase = $file.BaseName
+
+    #Assign Video/Audio codecs to variables
+    $ffprobe = Get-Command -CommandType Application -Name $ffmpegLoc\'ffprobe.exe' -ErrorAction SilentlyContinue
+    $ffprobe = $ffprobe.Source
+    $ffmpeg = Get-Command -CommandType Application -Name $ffmpegLoc\'ffmpeg.exe' -ErrorAction SilentlyContinue
+    $ffmpeg = $ffmpeg.Source
+
+    $vidC = $(& $ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "$fpath\$file")
+    
+    if ($file.Extension -ne '.srt' -Or $file.Extension -ne '.en.srt') {
+
+        #IF NOT H264 & MP4, CONVERT
+        if (($file.Extension -ne '.mp4' -AND $vidC -eq 'h264') -OR ($file.Extension -eq '.mp4' -AND $vidC -ne 'h264')) {
+            
+            & $ffmpeg -i $fpath\$file -c:v libx264 -preset fast -crf 22 -c:a aac "$fpath\$fileBase.mp4"
+            
+            #If transfer enabled and file exists, transfer
+            if ($enable_transfer -eq 1 -AND (Get-Item -Path "$fpath\$fileBase.mp4" )) {
+                transfer_file("$fileBase.mp4")
+            }
+        }    
+        else {
+            transfer_file("$file")
+        }
+            
+    }
+    else {
+        transfer_file("$file")
+    }
+}
 
 #Loop through files in fpath location and send to convert_file function for processing
-
 $files = Get-ChildItem -File "$fpath"
 foreach ($f in $files) {
     #File is movie
     if ($f.Extension -in $ext_array) {
-        Write-Host $f
-        #convert_file "$i"
+        convert_file($f)
     }
-    else{
-        #delete file
+    else {
+        Remove-Item $fpath\$f
     }
     
 }
 
 #If the movie path does not equal the save folder path delete movie folder
-if ("$fpath" -ne "$saveFolder") {
-    Write-Host "$fpath, $savefolder"
-    #rmdir "$m_fpath"
+if ("$fpath" -ne "$path") {
+    Remove-Item $fpath
 }
 
 Write-Host -NoNewLine 'Press any key to continue...';
